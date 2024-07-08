298c16fc2ef7c90a96e3b34507fa51623077da27
Aditya <gaditya110803@gmail.com>
2024-07-08 20:07:24 +0530

    commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-green-500'> SM</span>
+                <span>RI</span><span className='text-green-500'>TI</span>
+            </div>
+            <div className='flex justify-center items-center'> Created by Aditya </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-slate-800 text-white '>
+            <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+
+                <div className="logo font-bold text-white text-2xl">
+                    <span className='text-green-500'> SM</span>
+                    <span>RI</span><span className='text-green-500'>TI</span>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
